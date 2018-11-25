fc31950776873c3dccd9a6c841474efc5fe56131
Ricardo Pardini <ricardo@pardini.net>
2018-11-25 06:47:46 +0100

    call gpg early in macos build script

--- build_sign_osx.sh
+++ build_sign_osx.sh
@@ -2,8 +2,12 @@
 
 set -e
 
+# Make sure we can GPG sign stuff (eg, ask for yubikey PIN first)
+echo "not important" | gpg --sign --armor
+
 docker build -t adoptopenjdk/deb:latest .
 rm -rf to_sign
 docker run -it -v ${PWD}/to_sign/:/presign adoptopenjdk/deb:latest
 osx/debsign_osx.sh --no-conf -S to_sign/ubuntu/adoptopenjdk-java*-installer_*_source.changes
 docker run -it -v ${PWD}/to_sign/:/postsign adoptopenjdk/deb:latest
+
