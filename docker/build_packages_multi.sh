308497166abf238a3ca421968b294d3f2dba4c3f
Ricardo Pardini <ricardo@pardini.net>
2018-12-08 14:27:31 +0100

    dont build binaries or test them during source builds (for now)

--- docker/build_packages_multi.sh
+++ docker/build_packages_multi.sh
@@ -3,6 +3,9 @@
 set -e
 BASE_DIR=$(pwd)
 
+BUILD_BINARY_PACKAGES=false
+TEST_INSTALL_BINARY=false
+
 # Every directory under pwd should be a java version.
 for oneJavaVersion in *; do
   cd ${BASE_DIR}/${oneJavaVersion}
@@ -13,31 +16,31 @@ for oneJavaVersion in *; do
   for oneDistribution in *; do
     echo "Building Distribution: $oneDistribution"
 
-    echo "BINARY $oneJavaVersion $oneDistribution" | figlet
-    cd ${BUILD_BASE_DIR}/${oneDistribution}
-    debuild -us -uc # binary build, no signing.
-    cd ${BUILD_BASE_DIR}
+    if [[ "a$BUILD_BINARY_PACKAGES" == "atrue" ]]; then
+      echo "BINARY $oneJavaVersion $oneDistribution" | figlet
+      cd ${BUILD_BASE_DIR}/${oneDistribution}
+      debuild -us -uc # binary build, no signing.
+      cd ${BUILD_BASE_DIR}
 
-    # check if we can install these binaries. this serves as a basic sanity check.
-    # in practive this only "tests" amd64 packages, and for the distro in the FROM ubuntu:xxx
-    # line in the dockerfile, but is better than nothing.
-    #if [[ "${oneJavaVersion}" == "java-11" ]]; then
-      if [[ "$(lsb_release -c -s)" == "$oneDistribution" ]]; then
-        ls -la adoptopenjdk-*-installer_*_amd64.deb || true
-        dpkg -i adoptopenjdk-*-installer_*_amd64.deb
+      if [[ "a$TEST_INSTALL_BINARY" == "atrue" ]]; then
+        # check if we can install these binaries. this serves as a basic sanity check.
+        # in practice this only "tests" amd64 packages, and for the distro in the FROM ubuntu:xxx
+        # line in the dockerfile, but is better than nothing.
+        # @TODO: make this a separate step.
+        if [[ "$(lsb_release -c -s)" == "$oneDistribution" ]]; then
+          ls -la adoptopenjdk-*-installer_*_amd64.deb || true
+          dpkg -i adoptopenjdk-*-installer_*_amd64.deb
+        fi
       fi
-    #fi
 
-    mv -v adoptopenjdk* /binaries/
+      mv -v adoptopenjdk* /binaries/
+    fi
 
     echo "$oneJavaVersion $oneDistribution" | figlet
     cd ${BUILD_BASE_DIR}/${oneDistribution}
     debuild -S -us -uc # source-only build, no signing.
     cd ${BUILD_BASE_DIR}
-    # we dont need these .build or .buildinfo files, thanks.
 
-    # turns out DPUT needs these guys. I wonder why.
-    # rm adoptopenjdk*.build adoptopenjdk*.buildinfo || true # debian does not generate them, so ignore errors
     mv -v adoptopenjdk* /sourcepkg/
   done
 
