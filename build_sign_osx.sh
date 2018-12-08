6e15e4f89e075853bd3177be69981d3e4645baab
Ricardo Pardini <ricardo@pardini.net>
2018-12-08 15:37:59 +0100

    fix debsign invocation with new filename pattern

--- build_sign_osx.sh
+++ build_sign_osx.sh
@@ -8,6 +8,6 @@ echo "not important" | gpg --sign --armor
 docker build -t adoptopenjdk/deb:latest .
 rm -rf to_sign
 docker run -it -v ${PWD}/to_sign/:/presign adoptopenjdk/deb:latest
-osx/debsign_osx.sh --no-conf -S to_sign/ubuntu/adoptopenjdk-java*-installer_*_source.changes
+osx/debsign_osx.sh --no-conf -S to_sign/ubuntu/adoptopenjdk-*-installer_*_source.changes
 docker run -it -v ${PWD}/to_sign/:/postsign adoptopenjdk/deb:latest
 
