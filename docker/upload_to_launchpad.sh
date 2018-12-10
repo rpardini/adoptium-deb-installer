ac02235cd3ee29121d255f768af5c1bfe7e31817
Ricardo Pardini <ricardo@pardini.net>
2018-12-10 18:27:27 +0100

    oops, left dput commented out, fix

--- docker/upload_to_launchpad.sh
+++ docker/upload_to_launchpad.sh
@@ -4,10 +4,9 @@ set -e
 
 if [[ ! -d /to_upload/empty ]]; then
   echo "/to_upload is mounted. Uploading packages from there."
-  tree /to_upload
   cd /to_upload
   ls -la *_source.changes
-  # dput --unchecked ppa:rpardini/adoptopenjdk *_source.changes
+  dput --unchecked ppa:rpardini/adoptopenjdk *_source.changes
 else
   echo "Could not find /to_upload volume."  1>&2
   exit 1
