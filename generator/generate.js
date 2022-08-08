ddbc40c04a344cbc5c4e9dead2efa8674eb26cf0
Ricardo Pardini <ricardo@pardini.net>
2022-08-08 11:17:23 +0200

    out with impish, in with kinetic

--- generator/generate.js
+++ generator/generate.js
@@ -7,7 +7,7 @@ const wantedJavaVersions = new Set([8, 11, 16, 17, 18]); // official GA releases
 const linuxesAndDistros = new Set([
     {
         name: 'ubuntu',
-        distros: new Set(['trusty', 'xenial', 'bionic', 'focal', 'jammy', 'impish']), // LTS ESM releases + last release + next, but not the ones frozen, like hir.sute or gro.ovy
+        distros: new Set(['trusty', 'xenial', 'bionic', 'focal', 'jammy', 'kinetic']), // LTS ESM releases + last release + next, but not the ones frozen, like hir.sute or gro.ovy
         standardsVersion: "3.9.7",
         useDistroInVersion: true,
         singleBinaryForAllArches: false,
