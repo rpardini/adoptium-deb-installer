167b9ce253eae4530c4d6a8f58fdea7bd88b42b6
Ricardo Pardini <ricardo@pardini.net>
2022-12-04 12:51:42 +0100

    still spitting out zstd; try to debug

--- debuild.sh
+++ debuild.sh
@@ -13,31 +13,33 @@ cp -rp "generated/${os}/${jdkVersion}/${distribution}" "${BUILD_OUTPUT_DIR}"/
 cd "${BUILD_OUTPUT_DIR}/${distribution}"
 
 if [[ "${os}" == "debian" ]]; then
-  BUILD_SOURCE_PACKAGES=false
-  BUILD_BINARY_PACKAGES=true
+	BUILD_SOURCE_PACKAGES=false
+	BUILD_BINARY_PACKAGES=true
 fi
 
 if [[ "${os}" == "ubuntu" ]]; then
-  BUILD_SOURCE_PACKAGES=true
-  BUILD_BINARY_PACKAGES=false
+	BUILD_SOURCE_PACKAGES=true
+	BUILD_BINARY_PACKAGES=false
 fi
 
 if [[ "a$BUILD_BINARY_PACKAGES" == "atrue" ]]; then
-  echo "::group::BINARY $jdkVersion $distribution"
-  eatmydata debuild -Zxz # full build; use xz compression, not zstd (which is default in Ubuntu 22+)
-  echo "Build done! Moving packages..."
-  mkdir -p "${DEST_DIR}/binaries/"
-  mv -v "${BUILD_OUTPUT_DIR}"/adoptium* "${DEST_DIR}/binaries/"
-  echo "::endgroup::"
+	echo "::group::BINARY $jdkVersion $distribution"
+	eatmydata debuild -Zxz # full build; use xz compression, not zstd (which is default in Ubuntu 22+)
+	echo "Build done! Moving packages..."
+	mkdir -p "${DEST_DIR}/binaries/"
+	file "${BUILD_OUTPUT_DIR}"/adoptium* || true
+	mv -v "${BUILD_OUTPUT_DIR}"/adoptium* "${DEST_DIR}/binaries/"
+	echo "::endgroup::"
 fi
 
 if [[ "a$BUILD_SOURCE_PACKAGES" == "atrue" ]]; then
-  echo "::group::SOURCE $jdkVersion $distribution"
-  eatmydata debuild -S -Zxz # source-only build; use xz compression, not zstd (which is default in Ubuntu 22+)
-  echo "Build done! Moving packages..."
-  mkdir -p "${DEST_DIR}/sourcepkg/"
-  mv -v "${BUILD_OUTPUT_DIR}"/adoptium* "${DEST_DIR}/sourcepkg/"
-  echo "::endgroup::"
+	echo "::group::SOURCE $jdkVersion $distribution"
+	eatmydata debuild -S -Zxz # source-only build; use xz compression, not zstd (which is default in Ubuntu 22+)
+	echo "Build done! Moving packages..."
+	mkdir -p "${DEST_DIR}/sourcepkg/"
+	file "${BUILD_OUTPUT_DIR}"/adoptium* || true
+	mv -v "${BUILD_OUTPUT_DIR}"/adoptium* "${DEST_DIR}/sourcepkg/"
+	echo "::endgroup::"
 fi
 
 # Cleanup
